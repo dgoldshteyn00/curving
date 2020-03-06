343c14253a18d08e373f7cb28f70a3e6c9d2516a
dgoldshteyn00 <45824229+dgoldshteyn00@users.noreply.github.com>
2020-03-06 18:06:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matrix import *
 import math
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [0, 0, 0]
 edges = []
 transform = new_matrix()
 
@@ -19,4 +19,80 @@ transform = new_matrix()
 # print
 # print_matrix( make_rotZ(math.pi/4) )
 
-parse_file( 'script', edges, transform, screen, color )
+ARG_COMMANDS = ['line', 'scale', 'move', 'rotate', 'save', 'bezier', 'hermite', 'circle']
+STEP = 0.001
+
+"""
+def parse_file(fname, edges, transform, screen, color):
+    f = open(fname)
+    lines = f.readlines()
+
+    c = 0
+    while c < len(lines):
+        line = lines[c].strip()
+        # print ':' + line + ':'
+
+        if line in ARG_COMMANDS:
+            c += 1
+            args = lines[c].strip().split(' ')
+
+        if line == 'line':
+            # print 'LINE\t' + str(args)
+
+            add_edge(edges,
+                     float(args[0]), float(args[1]), float(args[2]),
+                     float(args[3]), float(args[4]), float(args[5]))
+
+        elif line == 'scale':
+            # print 'SCALE\t' + str(args)
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(t, transform)
+
+        elif line == 'move':
+            # print 'MOVE\t' + str(args)
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult(t, transform)
+
+        elif line == 'rotate':
+            # print 'ROTATE\t' + str(args)
+            theta = float(args[1]) * (math.pi / 180)
+
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult(t, transform)
+
+        elif line == 'ident':
+            ident(transform)
+
+        elif line == 'apply':
+            matrix_mult(transform, edges)
+
+        elif line == 'bezier':
+            add_curve(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]),
+                      float(args[5]), float(args[6]), float(args[7]), STEP, 'bezier')
+
+        elif line == 'hermite':
+            add_curve(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), float(args[4]),
+                      float(args[5]), float(args[6]), float(args[7]), STEP, 'hermite')
+
+        elif line == 'circle':
+            add_circle(edges, float(args[0]), float(args[1]), float(args[2]), float(args[3]), STEP)
+
+        elif line == 'display' or line == 'save':
+            clear_screen(screen)
+            draw_lines(edges, screen, color)
+
+            if line == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
+                print(args[0])
+
+        c += 1
+"""
+
+parse_file('script', edges, transform, screen, color)
